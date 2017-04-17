67255b35b999daf21bc9b5a67ce0b1b5b509fcb3
jjd <jjdambrosio@gmail.com>
2017-04-17 09:09:37 -0300

    Lean Caller and LeanSTP added

---
+++ LeanSTP/LeanWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace QuantConnect.Lean.LeanSTP
+{
+    public class LeanWorker : MarshalByRefObject 
+    {
+        public void RunAlgorithm(object[] args)
+        {
+            var command = string.Format("Caller {0} {1} {2} {3}", args[0], args[1], args[2], args[3]);
+            using (var p = new Process())
+            {
+                p.StartInfo.WorkingDirectory = @"D:\REPOS\LeanSTP\Caller\bin\Debug";
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.FileName = "CMD.exe";
+                p.Start();
+                using (StreamWriter sw = p.StandardInput)
+                {
+                    if (sw.BaseStream.CanWrite)
+                    {
+                        sw.WriteLine(command);
+                    }
+                }
+                p.WaitForExit();
+            }
+
+        }
+    }
+}
---
+++ LeanSTP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantConnect.Lean.LeanSTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leanSTP = new LeanSTP("ParameterizedAlgorithm", Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments));
+            leanSTP.Start();
+        }
+    }
+}
