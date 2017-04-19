1ebb578b4d1b5c5b095e3782e1b247b020ba100b
jjd <jjdambrosio@gmail.com>
2017-04-19 11:32:00 -0300

    dead end... bad ideas + bad implementations

--- Algorithm.CSharp/QCForumAlgorithms/TimeSeriesMomentumEffect.cs
+++ Algorithm.CSharp/QCForumAlgorithms/TimeSeriesMomentumEffect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using QuantConnect.Brokerages;
 using QuantConnect.Data;
 using QuantConnect.Data.Custom;
 using QuantConnect.Indicators;
@@ -16,14 +17,17 @@ namespace QuantConnect.Algorithm.CSharp
     /// <seealso cref="QuantConnect.Algorithm.QCAlgorithm" />
     public class TimeSeriesMomentumEffect : QCAlgorithm
     {
+        // The Generally the 10 year maturity is used as a proxy of the risk free rate.
+        private const string riskFreeReturnQuandlCode = "USTREASURY/YIELD";
+
+        private const int forexLeverage = 20;
+        private const string forexMarket = "oanda"; // "fxcm"
+        private const int volatilityWindow = 90;
         private readonly Dictionary<Symbol, decimal> excessReturns = new Dictionary<Symbol, decimal>();
 
         private readonly string[] forexTickers =
         {
-            "AUDUSD",
-            "EURUSD",
-            "GBPUSD",
-            "USDJPY"
+            "EURUSD", "USDJPY", "USDCHF", "GBPUSD", "USDCAD", "AUDUSD"
         };
 
         // The full list of Futures can be found in Table 1
@@ -54,22 +58,24 @@ namespace QuantConnect.Algorithm.CSharp
             Futures.Softs.Sugar11
         };
 
-        // The Generally the 10 year maturity is used as a proxy of the risk free rate.
-        private readonly string riskFreeReturnQuandlCode = "USTREASURY/YIELD";
-
-        // 
         private readonly List<Symbol> symbols = new List<Symbol>();
+        private DateTime LastTradableDayInMonth;
 
-        private bool monthlyRebalance;
+        private bool rebalancePortfolio;
+
 
         private decimal riskFreeRetun;
 
         public override void Initialize()
         {
             // Set the basic algorithm parameters.
-            SetStartDate(2016, 06, 01);
-            SetEndDate(2016, 08, 30);
+            SetStartDate(2008, 01, 01);
+            SetEndDate(2017, 03, 30);
             SetCash(100000);
+
+            BrokerageName brokerage;
+            Enum.TryParse(forexMarket, out brokerage);
+            SetBrokerageModel(brokerage);
 
             AddData<QuandlUSTeasuryYield>(riskFreeReturnQuandlCode, Resolution.Daily);
 
@@ -78,47 +84,71 @@ namespace QuantConnect.Algorithm.CSharp
 
             foreach (var ticker in forexTickers)
             {
-                var forex = AddForex(ticker, Resolution.Daily, Market.Oanda);
+                var forex = AddForex(ticker, Resolution.Daily, forexMarket, leverage: forexLeverage);
                 symbols.Add(forex.Symbol);
                 // https://en.wikipedia.org/wiki/Coefficient_of_variation
                 forex.VolatilityModel =
-                    new IndicatorVolatilityModel<IndicatorDataPoint>(STD(forex.Symbol, 20, Resolution.Daily)
-                        .Over(SMA(forex.Symbol, 20, Resolution.Daily)));
+                    new IndicatorVolatilityModel<IndicatorDataPoint>(
+                        STD(forex.Symbol, volatilityWindow, Resolution.Daily)
+                            .Over(SMA(forex.Symbol, volatilityWindow, Resolution.Daily)));
             }
 
+            Schedule.On(DateRules.MonthStart(), TimeRules.At(0, 0), GetLastTradableDayInMonth);
 
-            Schedule.On(DateRules.MonthStart(), TimeRules.At(0, 0), () =>
-            {
-                UpdateAssetsReturns();
-                monthlyRebalance = true;
-            });
+            SetWarmUp(TimeSpan.FromDays(volatilityWindow));
         }
 
         private void UpdateAssetsReturns()
         {
-            var dateRequest = new DateTime(Time.Year - 1, Time.Month, Time.Day);
+            var requestDay = Time.Day;
+            if (DateTime.IsLeapYear(Time.Year) && Time.Month == 2) requestDay--;
+            var dateRequest = new DateTime(Time.Year - 1, Time.Month, requestDay);
             // I ask for some days before just in case the selected day hasn't historical prices record.
             var history = History(symbols, dateRequest.AddDays(-5), dateRequest.AddDays(1), Resolution.Daily);
             foreach (var symbol in symbols)
-            {
-                Slice slice;
                 try
                 {
-                    slice = history.Last(s => s.ContainsKey(symbol));
+                    var slice = history.Last(s => s.ContainsKey(symbol));
                     excessReturns[symbol] = (Securities[symbol].Price / slice[symbol].Price - 1m) * 100m -
                                             riskFreeRetun;
                 }
-                catch (Exception e)
+                catch (Exception ex)
                 {
-                    Console.WriteLine(e);
+                    Console.WriteLine(symbol.Value, " hasn't data to estimate excess returns.");
                     excessReturns[symbol] = 0m;
                 }
+        }
+
+        private void GetLastTradableDayInMonth()
+        {
+            var lastDayinMonth = new DateTime(Time.Year, Time.Month, DateTime.DaysInMonth(Time.Year, Time.Month));
+            switch (lastDayinMonth.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                    lastDayinMonth = lastDayinMonth.AddDays(-1);
+                    break;
+                case DayOfWeek.Sunday:
+                    lastDayinMonth = lastDayinMonth.AddDays(-2);
+                    break;
             }
+            LastTradableDayInMonth = lastDayinMonth;
         }
 
         public override void OnData(Slice slice)
         {
-            if (monthlyRebalance)
+            if (IsWarmingUp) return;
+
+            // Check if today is the month last day
+            if (Time.Date == LastTradableDayInMonth)
+            {
+                // Liquidate all existing holdings.
+                Liquidate();
+                UpdateAssetsReturns();
+                rebalancePortfolio = true;
+                return;
+            }
+
+            if (rebalancePortfolio)
             {
                 var assetsToLong = excessReturns.Where(s => s.Value > 0m)
                     .ToDictionary(pair => pair.Key,
@@ -127,23 +157,15 @@ namespace QuantConnect.Algorithm.CSharp
                     .ToDictionary(pair => pair.Key,
                         pair => Securities[pair.Key].VolatilityModel.Volatility);
 
-                var tradesToClose = Securities.Where(
-                    secPair => Portfolio[secPair.Key].Invested &&
-                                   !assetsToShort.Keys.Concat(assetsToLong.Keys).Contains(secPair.Key))
-                                               .Select(s => s.Key);
-
-                foreach (var symbol in tradesToClose)
-                {
-                    Liquidate(symbol);
-                }
 
                 var divisor = assetsToLong.Values.Sum();
                 foreach (var keyValuePair in assetsToLong)
                     SetHoldings(keyValuePair.Key, keyValuePair.Value / divisor);
+
                 divisor = assetsToShort.Values.Sum();
                 foreach (var keyValuePair in assetsToShort)
                     SetHoldings(keyValuePair.Key, -keyValuePair.Value / divisor);
-                monthlyRebalance = false;
+                rebalancePortfolio = false;
             }
         }
 
