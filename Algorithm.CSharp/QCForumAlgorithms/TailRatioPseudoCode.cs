13c3b3534c6d1374cdc6ff8585d7b0d15bb75dc2
jjd <jjdambrosio@gmail.com>
2017-04-28 08:39:10 -0300

    tailratio pseudo-code added

---
+++ Algorithm.CSharp/QCForumAlgorithms/TailRatioPseudoCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.Statistics;
+using QuantConnect.Indicators;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class TailRatioPseudoCode : QCAlgorithm
+    {
+        private List<double> dailyResults;
+        private LogReturn equityLogReturn;
+
+        public override void Initialize()
+        {
+            equityLogReturn = new LogReturn(1);
+            dailyResults = new List<double>();
+        }
+
+        public override void OnEndOfDay()
+        {
+            equityLogReturn.Update(new IndicatorDataPoint
+            {
+                Time = Time.Date,
+                Value = Portfolio.TotalPortfolioValue
+            });
+            dailyResults.Add((double) equityLogReturn.Current.Value);
+        }
+
+        public override void OnEndOfAlgorithm()
+        {
+            var tailRatio = dailyResults.Percentile(95) / Math.Abs(dailyResults.Percentile(5));
+        }
+    }
+}
